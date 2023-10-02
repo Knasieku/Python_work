eef43c0d70727846fc2d04374c390613d5d39648
Knasieku <knasieku@gmail.com>
2023-10-02 16:36:32 +0300

    added sign up to geekbooks

--- python_work/try.py
+++ python_work/try.py
@@ -1,5 +1,14 @@
-def name():
-    your_name=input()
-    print("your name is "+ your_name)
+# def name():
+#     your_name=input()
+#     print("your name is "+ your_name)
+# name()
 
-name()
+
+def geekbooks():
+      print("Welcome to geekbooks to sign up enter" )
+      user_name=input()
+      password=input()
+      email=input()
+      print("Kindly confirm your username is " + user_name + "Your password is "+password +"your email is "+email)
+geekbooks()
+
--- try.py
+++
@@ -1,5 +0,0 @@
-def name():
-    your_name=input()
-    print("your name is "+ your_name)
-
-name()
