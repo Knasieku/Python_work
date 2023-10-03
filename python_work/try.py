cb9d52c443f4b66d9204dd7b3d00d51ffdf47688
Knasieku <knasieku@gmail.com>
2023-10-03 15:38:46 +0300

    added password rules

--- python_work/try.py
+++ python_work/try.py
@@ -4,11 +4,42 @@
 # name()
 
 
-def geekbooks():
-      print("Welcome to geekbooks to sign up enter" )
-      user_name=input()
-      password=input()
-      email=input()
-      print("Kindly confirm your username is " + user_name + "Your password is "+password +"your email is "+email)
-geekbooks()
+# def geekbooks():
+#       print("Welcome to geekbooks to sign up enter" )
+#       user_name=input()
+#       password=input()
+#       email=input()
+#       print("Kindly confirm your username is " + user_name + "Your password is "+password +"your email is "+email)
+# geekbooks()
 
+# #operators
+# # equals ==
+# if 5 == 3:
+#     print("5 equals to 3")
+# else:
+#     print("not equal")
+
+# not equals !=
+print("Enter your password: ")
+password=input()
+print("Confirm your password: ")
+confirm_password=input()
+
+if password != confirm_password:
+    print("password is not the same")
+
+# less than < and less than or equal to <=
+if 10<=2:
+    print("10 is less")
+elif 10<2:
+    print("10 maybe equal to")
+else:
+    print("equal")
+
+# greater than > and greater than or equal to >=
+if 10>=20:
+    print("10 is more")
+elif 10>2:
+    print("10 maybe equal to")
+else:
+    print("equal")
